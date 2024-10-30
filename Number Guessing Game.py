646e8bb2b7125c6dc62b9d581a698af52642e43a
RileyTernes <riley.ternes@gmail.com>
2024-10-30 13:42:43 -0500

    Create Number Guessing Game.py

---
+++ Number Guessing Game.py
@@ -0,0 +1,20 @@
+#Number Guessing Game
+def main():
+    #calls all of the functions
+    #drives program
+    #recieves no arguments
+    menu()
+    name()
+    random_integer(range)
+    
+    
+def menu():
+    #displays a menu
+    #sends 
+    return selection
+def name():
+    
+def random_integer(range):
+    
+    
+def take_turns():
