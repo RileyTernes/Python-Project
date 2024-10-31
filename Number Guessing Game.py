edca55b80dcb7952ba7c200662f59add6a11eb42
RileyTernes <riley.ternes@gmail.com>
2024-10-31 13:15:53 -0500

    Update Number Guessing Game.py

--- Number Guessing Game.py
+++ Number Guessing Game.py
@@ -1,20 +1,22 @@
 #Number Guessing Game
+import random
 def main():
     #calls all of the functions
     #drives program
     #recieves no arguments
     menu()
     name()
-    random_integer(range)
-    
+    random_integer(range_low, range_high)
+    take_turns(random_number, player1, player2)
     
 def menu():
     #displays a menu
-    #sends 
     return selection
 def name():
-    
-def random_integer(range):
-    
-    
-def take_turns():
+    player1 = input("Please enter the first player's name: ")
+    player2 = input("Please enter the second player's name: ")
+    return player1, player2
+def random_integer(range_low, range_high):
+    random_number = random.randint(range_low, range_high)
+def take_turns(random_number, player1, player2):
+    asd
