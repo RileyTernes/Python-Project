9905362bf79fc5858d5b9dfb6f2c930271b04437
RileyTernes <riley.ternes@gmail.com>
2024-10-31 13:47:27 -0500

    Update Number Guessing Game.py

--- Number Guessing Game.py
+++ Number Guessing Game.py
@@ -1,13 +1,19 @@
 #Number Guessing Game
 import random
 def main():
+    #set default range
+    range_low = 1
+    range_high = 1000
     #calls all of the functions
     #drives program
     #recieves no arguments
     menu()
-    name()
-    random_integer(range_low, range_high)
-    take_turns(random_number, player1, player2)
+    if selection == "start":
+        name()
+        random_integer(range_low)
+        take_turns(random_number, player1, player2)
+    if selection == "range":
+        
     
 def menu():
     #displays a menu, 1. Start New Game, 2. Choose Range, 3. Exit.
@@ -16,10 +22,11 @@ def menu():
     print("Choose option by typing in number")
     number = input(f"1. Start New Game \n 2.Choose range \n 3. Exit")
     if number == 1:
-        selection = name
+        selection = "start"
     if number == 2:
-        selection = random_integer
-    
+        selection = "range"
+    else:
+        quit()
     #displays a menu
     return selection
 def name():
@@ -29,3 +36,7 @@ def name():
 def random_integer(range_low, range_high):
     random_number = random.randint(range_low, range_high)
 def take_turns(random_number, player1, player2):
+
+def range_getter():
+    range_low == int(input("Please enter the floor for the range: "))
+    range_high == int(input(Please enter the ceiling for the range: "))
