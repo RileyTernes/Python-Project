5f4ab4a3ac5a240ae06bf5fc1f608df6bd340f99
AsherRinger <aringersks@gmail.com>
2024-10-31 13:34:19 -0500

    Menu complete
    Completed the menu for project

--- Number Guessing Game.py
+++ Number Guessing Game.py
@@ -9,8 +9,16 @@ def main():
     
     
 def menu():
-    #displays a menu
-    #sends 
+    #displays a menu, 1. Start New Game, 2. Choose Range, 3. Exit.
+    #function returns fallowing functions based upon function chosen.
+    
+    print("Choose option by typing in number")
+    number = input(f"1. Start New Game \n 2.Choose range \n 3. Exit")
+    if number == 1:
+        selection = name
+    if number == 2:
+        selection = random_integer
+    
     return selection
 def name():
     
